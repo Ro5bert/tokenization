0353b803f3ebe109c1f1b95aea816b0e3f1f3cd3
Ro5bert <robertrussell.72001@gmail.com>
2018-07-01 11:33:53 -0700

    after pypi upload

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='tokenization',
     version='1.0.0',
     packages=find_packages(),
-    url='',
+    url='https://github.com/Ro5bert/tokenization',
     license='MIT',
     author='Robert Russell',
     author_email='robertrussell.72001@gmail.com',
