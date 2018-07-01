3658bffbe247ead350c4a4b86968c0667be4526f
Robert Russell <robertrussell.72001@gmail.com>
2018-07-01 10:50:41 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tokenization',
+    version='1.0.0',
+    packages=find_packages(),
+    url='',
+    license='MIT',
+    author='Robert Russell',
+    author_email='robertrussell.72001@gmail.com',
+    description='A general purpose text tokenizing module for python.',
+    classifiers=(
+        "Topic :: Text Processing",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    )
+)
